d2124f28ee1d917b753cdf9938f0cf4c40918f5b
SurfaceSystems <116438025+SurfaceSystems@users.noreply.github.com>
2022-12-07 13:11:24 +0100

    Update 1.1
    Fixed problems in find function and added new functions.

--- arrays.cpp
+++ arrays.cpp
@@ -15,6 +15,58 @@ bool findStr(std::string array[], std::string find) {
     }
     return false;
 }
+bool findInt(int array[], int find) {
+    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        if(array[i] == find) {
+            return true;
+        }
+    }
+    return false;
+}
+bool findBoo(bool array[], bool find) {
+    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        if(array[i] == find) {
+            return true;
+        }
+    }
+    return false;
+}
+bool findSho(short array[], short find) {
+    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        if(array[i] == find) {
+            return true;
+        }
+    }
+    return false;
+}
+bool findLon(long array[], long find) {
+    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        if(array[i] == find) {
+            return true;
+        }
+    }
+    return false;
+}
+bool findCha(char array[], char find) {
+    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        if(array[i] == find) {
+            return true;
+        }
+    }
+    return false;
+}
+bool findFlo(float array[], float find) {
+    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        if(array[i] == find) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
 
 std::string scanStr(std::string array[], char separator) {
     std::string returnText;
@@ -56,6 +108,14 @@ std::string scanCha(char array[], char separator) {
     std::string returnText;
     for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
         returnText += array[i] + separator + " ";
+    }
+    return returnText;
+}
+
+std::string scanFlo(float array[], char separator) {
+    std::string returnText;
+    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        returnText += to_string(array[i]) + separator + " ";
     }
     return returnText;
 }
